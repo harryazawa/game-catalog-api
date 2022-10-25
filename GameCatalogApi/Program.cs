1a7f36f3822315f82b08c39c6172b2e48a29ee5d
harryazawa <victorlbamorim@gmail.com>
2022-10-25 17:38:18 -0300

    feat: added LifeCycleIDController and Services specifications for LifeCycle on Program.cs

--- GameCatalogApi/Program.cs
+++ GameCatalogApi/Program.cs
@@ -1,3 +1,4 @@
+using GameCatalogApi.Controllers.V1;
 using GameCatalogApi.Repositories;
 using GameCatalogApi.Services;
 
@@ -7,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddScoped<IGameRepository, GameRepository>();
+// In case it's preferable to use SQLServer, just change the line above for the line bellow:
+// builder.Services.AddScoped<IGameRepository, GameSqlServerRepository>();
+
+#region LifeCycle
+
+builder.Services.AddSingleton<LifeCycleIDController.IExampleSingleton, LifeCycleIDController.LifeCycleExample>();
+builder.Services.AddScoped<LifeCycleIDController.IExampleScoped, LifeCycleIDController.LifeCycleExample>();
+builder.Services.AddTransient<LifeCycleIDController.IExampleTransient, LifeCycleIDController.LifeCycleExample>();
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
